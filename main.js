e995f922bda87a5192bc6ad0a4ac6adaeb2f8cc3
Nwachukwu Ugwuanyi <codedeus>
2018-02-07 13:15:45 +0100

    trying compression

--- main.js
+++ main.js
@@ -4,17 +4,7 @@ var compression = require('compression');
 const PORT = process.env.PORT || 5000
 
 var app = express();
+app.use(compression());
 app.use(express.static(path.join(__dirname, '/')));
-app.use(compression({filter: shouldCompress}));
 
-function shouldCompress (req, res) {
-  if (req.headers['x-no-compression']) {
-	// don't compress responses with this request header
-	return false
-  }
-
-  // fallback to standard filter function
-  return compression.filter(req, res)
-}
-  
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
